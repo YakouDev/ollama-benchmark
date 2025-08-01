2f5f7f5e57d63962b6966c7f734fffb66cf9c7b1
Yakou Developer <yakoudev@gmail.com>
2025-08-01 20:55:18 +0700

    Update obench.sh

--- obench.sh
+++ obench.sh
@@ -24,16 +24,10 @@ green='\e[0;32m'
 red='\e[0;31m'
 blue='\e[0;34m'
 NC='\e[0m' # No Color
-cpu_def=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
-echo "Setting cpu governor to"
-sudo echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-gpu_avail=$(sudo lshw -C display | grep product: | head -1 | cut -c17-)
-cpugover=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
 cpu_used=$(lscpu | grep 'Model name' | cut -f 2 -d ":" | awk '{$1=$1}1')
 echo ""
 echo "Simple benchmark using ollama and"
 echo "whatever local Model is installed."
-echo "Does not identify if $gpu_avail is benchmarking"
 echo ""
 echo "How many times to run the benchmark?"
 read benchmark
@@ -51,11 +45,11 @@ ollama show $model --system
 echo ""
 echo -e "Will use model: "${green}$model${NC}
 echo ""
-echo -e Will benchmark the tokens per second for ${cpu_used} and or ${gpu_avail}
+echo -e Will benchmark the tokens per second for ${cpu_used}
 touch "${cpu_used}".txt
 echo "" > "${cpu_used}".txt
 echo ""
-echo -e Running benchmark ${purple}$benchmark${NC} times for ${cpu_used} and or ${gpu_avail}
+echo -e Running benchmark ${purple}$benchmark${NC} times for ${cpu_used}
 echo -e with ${borange}$cpugover${NC} setting for cpu governor
 echo ""
 for run in $(seq 1 $benchmark);
@@ -65,11 +59,9 @@ avg=$(cat "${cpu_used}".txt | grep -v "prompt eval rate:" | awk '{print $3}' | a
 done
 echo ""
 echo -e ${red}$avg${NC} is the average ${blue}tokens per second${NC} using ${green}$model${NC} model
-echo for $cpu_used and or $gpu_avail
+echo for $cpu_used
 echo -e using ${borange}$cpugover${NC} for cpu governor.
 echo ""
-echo "Setting cpu governor to"
-sudo echo $cpu_def | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 #comment this out if you are repeating the same model
 #this clears model from Vram
 sudo systemctl stop ollama; sudo systemctl start ollama
